1c691b6aabf31588a5b6f2bfcf200bcfddccba73
Didier Peran Ganthier <didierganthierperan@gmail.com>
2024-08-01 21:31:54 -0400

    Fix return

--- app/page.tsx
+++ app/page.tsx
@@ -4,45 +4,24 @@ import fetchPlayers from "./utils/api/fetchPlayers";
 
 export default function Home() {
   const [players, setPlayers] = useState<any[]>([]);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [isLoading, setIsLoading] = useState(false);
-  const [hasMore, setHasMore] = useState(true);
 
   useEffect(() => {
-    const fetchData = async () => {
-      setIsLoading(true);
-      try {
-        const data = await fetchPlayers(currentPage);
-        setPlayers((prevPlayers) => [...prevPlayers, ...data.data]);
-        setCurrentPage(currentPage + 1);
-        setHasMore(!!data.meta.next_cursor);
-      } catch (error) {
-        console.error(error);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    if (hasMore) {
-      fetchData();
-    }
-  }, [currentPage, hasMore]);
-
-  const handleLoadMore = () => {
-    setCurrentPage(currentPage + 1); // Trigger data fetch for next page
-  };
+    fetchPlayers().then((data: any) => {
+      console.log(data.data);
+      setPlayers(data.data);
+    }).catch((error: any) => {
+      console.error(error);
+    });
+  }, []);
 
   return (
     <div>
       <h1>NBA Players</h1>
-      {isLoading && <p>Loading...</p>}
       <ul>
         {players.map((player) => (
           <li key={player.id}>{player.first_name} {player.last_name}</li>
         ))}
       </ul>
-      {hasMore && <button onClick={handleLoadMore}>Load More</button>}
-      {!hasMore && <p>No more players to load.</p>}
     </div>
   );
 }
--- app/utils/api/fetchPlayers.ts
+++ app/utils/api/fetchPlayers.ts
@@ -1,21 +1,21 @@
-async function fetchPlayers(page = 1) {
-    const apiKey = process.env.NEXT_PUBLIC_NBA_API_KEY;
-  
-    const url = `https://api.balldontlie.io/v1/players?page=${page}`;
-    const response = await fetch(url, {
-      method: 'GET',
-      headers: {
-        'Authorization': `${apiKey}`,
-      },
-    });
-  
-    if (!response.ok) {
-      throw new Error(`Network response was not ok`);
-    }
-  
-    const data = await response.json();
-    return data;
+async function fetchPlayers() {
+  const apiKey = process.env.NEXT_PUBLIC_NBA_API_KEY; // Replace with your actual key
+
+  const response = await fetch('https://api.balldontlie.io/v1/players', {
+    method: 'GET',
+    headers: {
+      'Authorization': `${apiKey}`,
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error(`Network response was not ok`);
   }
 
-  export default fetchPlayers;
-  
+  const data = await response.json();
+  // Use the data from the response here
+  console.log(data);
+  return data;
+}
+
+export default fetchPlayers;
